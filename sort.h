291dd19fd464a6eea9b35185fce8b3c57b1227f2
razanalk <13238@holbertonstudents.com>
2026-05-04 08:18:26 +0000

    Add sort.h

---
+++ sort.h
@@ -0,0 +1,12 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stddef.h>
+
+/* function prototype */
+void bubble_sort(int *array, size_t size);
+
+/* provided function */
+void print_array(const int *array, size_t size);
+
+#endif

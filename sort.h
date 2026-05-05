ce3f25555b2a049c79504a2ce0a7ed28c06bf37f
shahadeissaalharbi <shahadeissa111@gmail.com>
2026-05-05 11:41:44 +0000

    Add selection_sort prototype to sort.h

--- sort.h
+++ sort.h
@@ -22,6 +22,8 @@ void bubble_sort(int *array, size_t size);
 /* insertion sort */
 void insertion_sort_list(listint_t **list);
 
+/* selection sort */
+void selection_sort(int *array, size_t size);
 /* provided functions */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);

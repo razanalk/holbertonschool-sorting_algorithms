984ab481fcea4231d073dc39a5223b3ff1868578
shahadeissaalharbi <shahadeissa111@gmail.com>
2026-05-05 11:43:44 +0000

    Add quick sort algorithm and Big O notations

--- sort.h
+++ sort.h
@@ -23,6 +23,7 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 
 /* selection sort */
+void quick_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
 /* provided functions */
 void print_array(const int *array, size_t size);
